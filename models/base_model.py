f69af4cd874d78588abccc9f965457ebd14a59ea
KevinTMO <kevin.ramos3@hotmail.com>
2021-06-29 03:19:41 +0000

    Updated

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,6 @@ Methods included:
 
 
 from datetime import datetime
-import json
 import uuid
 
 
@@ -22,13 +21,27 @@ class BaseModel:
     """
     create a base class
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         initialize all public instances
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+        if kwargs:
+            for key, val in kwargs.items():
+                if "created_at" == key:
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "updated_at" == key:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "__class__" == key:
+                    pass
+
+                else:
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -42,7 +55,6 @@ class BaseModel:
         updating the updated_at datetime
         """
         self.updated_at = datetime.now().isoformat()
-        return self.updated_at
 
     def to_dict(self):
         """
